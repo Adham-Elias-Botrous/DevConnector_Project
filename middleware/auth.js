f96ff3a7971d04971e22f69f3c162b0261a4a9ad
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-11-13 11:30:56 +0100

    client node_modules

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = async function(req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // if there is NO token => send response
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // if there is token => verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
+  }
+};
